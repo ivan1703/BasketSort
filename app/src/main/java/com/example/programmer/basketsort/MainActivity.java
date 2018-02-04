76f4e8128d78da1b54c83edfc612fbdaefcb7969
ivan1703 <iwan.balaschow228@mail.ru>
2018-02-04 19:45:34 +0300

    создал основные активити

--- app/src/main/java/com/example/programmer/basketsort/MainActivity.java
+++ app/src/main/java/com/example/programmer/basketsort/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.programmer.basketsort;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        Button starts =findViewById(R.id.StartProg);
+        starts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.content);
+            }
+        });
     }
 }
